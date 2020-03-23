490b21604b447c3a50377146f232777367f2b7d8
Catharina <catharinavas@gmail.com>
2020-03-23 10:19:24 -0300

    retrieve modal data

--- app/controllers/share_feedback/save_feedback.rb
+++ app/controllers/share_feedback/save_feedback.rb
@@ -10,7 +10,7 @@ def save_feedback(token_slack, token_monday, feature, payload)
   @user_id = @payload['user']['id']
 
   Thread.new do
-    retrive_modal_data
+    retrive_modal_data(feature)
     save_on_monday(feature).status == 200 ? post_thread : post_error_thread
   end
 
@@ -18,15 +18,30 @@ def save_feedback(token_slack, token_monday, feature, payload)
   response
 end
 
-def retrive_modal_data
+def retrive_modal_data(feature)
   @summary = @payload['view']['state']['values']['summary']['input']['value']
+
+  puts JSON.pretty_generate(@payload['view'])
+
+  @feedback_from = @payload['view']['state']['values']['dropdown']['input']['value']
+  @importance = @payload['view']['state']['values']['status7']['input']['value']
+
+  # MAKE RETRIEVE BASED ON CONFIG
+  # SAVE COLUMNS ON MONDAY
 end
 
 def save_on_monday(feature)
   HTTP.auth(@token_monday)
       .headers('content-type' => 'application/json')
       .post('https://api.monday.com/v2/', json:
-        { "query": "mutation{ create_item (board_id: #{feature.board_id}, group_id: #{feature.new_items_group}, item_name: \"#{@summary}\"){id}}" })
+        { "query": "mutation{ create_item (
+          board_id: #{feature.board_id},
+          group_id: #{feature.new_items_group},
+          item_name: \"#{@summary}\",
+          columns_values: {
+            dropdown: \"#{@feedback_from}\",
+            status7: \"#{@importance}\"
+          }){id}}" })
 end
 
 def post_thread
